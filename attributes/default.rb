e1e03ac1b8cfa234ee511635ed74d93198542345
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-06-17 15:06:22 +0100

    This is now working but there are no tests yet!

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['Subread']['version'] = '1.4.6-p1'
-default['Subread']['dirname'] = "subread-#{node['Subread']['version']}"
-default['Subread']['filename'] = "#{node['Subread']['dirname']}-Linux-x86_64.tar.gz"
-default['Subread']['url'] = "http://sourceforge.net/projects/subread/files/#{node['Subread']['dirname']}/#{node['Subread']['filename']}/download"
-default['Subread']['install_path'] = '/usr/local/bin/'
+default['Subread']['dirname'] = "subread-#{node['Subread']['version']}-source"
+default['Subread']['filename'] = "#{node['Subread']['dirname']}.tar.gz"
+default['Subread']['url'] = "http://sourceforge.net/projects/subread/files/subread-#{node['Subread']['version']}/#{node['Subread']['filename']}/download"
+default['Subread']['install_path'] = '/usr/local'
+default['Subread']['bin_path'] = '/usr/local/bin'
--- recipes/source.rb
+++ recipes/source.rb
@@ -4,10 +4,17 @@
 #
 # Copyright 2015, Eagle Genomics Ltd, All Rights Reserved.
 
+log 'Starting Subread recipe'
+
 include_recipe 'build-essential'
 
-remote_file "#{Chef::Config[:file_cache_path]}/#{node['Subread']['url']}" do
+package ['zlib-devel'] do
+    action :install
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['Subread']['filename']}" do
   source node['Subread']['url']
+  action :create_if_missing
 end
 
 execute "cp #{Chef::Config[:file_cache_path]}/#{node['Subread']['filename']} #{node['Subread']['install_path']}" do
@@ -16,8 +23,17 @@ execute "cp #{Chef::Config[:file_cache_path]}/#{node['Subread']['filename']} #{n
 end
 
 execute "tar zxvf #{node['Subread']['filename']}" do
-  cwd "#{node['Subread']['install_path']}"
+  cwd node['Subread']['install_path']
   not_if { ::File.exist?("#{node['Subread']['install_path']}/#{node['Subread']['dirname']}") }
 end
 
+execute "make -f Makefile.Linux" do
+  cwd "#{node['Subread']['install_path']}/#{node['Subread']['dirname']}/src"
+  not_if { ::File.exist?('../bin/exactSNP') }
+end
 
+execute "ln -s #{node['Subread']['install_path']}/#{node['Subread']['dirname']}/bin/* ." do
+  cwd node['Subread']['bin_path']
+end
+
+log 'Finished Subread recipe'
