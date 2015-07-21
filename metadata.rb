c764c8fb587aac29ef8c48dae89949e31eea9c4f
Nick James <nick.james@eaglegenomics.com>
2015-07-21 16:33:03 +0100

    updated metadata cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'all_rights'
 description 'Installs/Configures Subread'
 long_description 'Installs/Configures Subread'
-version '0.1.0'
+version '1.0.0'
 
 supports 'centos', '= 6.5'
 
