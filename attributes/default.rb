efc1a83e254d7fa8effaf3044aba1b5d753396d7
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-10-21 09:34:25 +0100

    Upgraded to Release 1.4.6-p5, 4 Sept 2015

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['Subread']['version'] = '1.4.6-p1'
+default['Subread']['version'] = '1.4.6-p5'
 default['Subread']['dirname'] = "subread-#{node['Subread']['version']}-source"
 default['Subread']['filename'] = "#{node['Subread']['dirname']}.tar.gz"
 default['Subread']['url'] = "http://sourceforge.net/projects/subread/files/subread-#{node['Subread']['version']}/#{node['Subread']['filename']}/download"
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'all_rights'
 description 'Installs/Configures Subread'
 long_description 'Installs/Configures Subread'
-version '1.0.0'
+version '1.0.1'
 
 supports 'centos', '= 6.5'
 
