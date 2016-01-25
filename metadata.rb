3eb83928d82e06fc934f6b7bd70c11aac3308aa6
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 12:35:10 +0000

    License, maintainers, description

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'Subread'
-maintainer 'The Authors'
+maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'all_rights'
+license 'apache2'
 description 'Installs/Configures Subread'
-long_description 'Installs/Configures Subread'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
 supports 'centos', '= 6.5'
