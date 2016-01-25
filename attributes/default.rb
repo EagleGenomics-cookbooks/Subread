d1e6d7bcc97bcc21293c61e5df82d15992fd42b5
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 13:30:19 +0000

    URL correction

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['Subread']['version'] = '1.4.6-p5'
 default['Subread']['dirname'] = "subread-#{node['Subread']['version']}-source"
 default['Subread']['filename'] = "#{node['Subread']['dirname']}.tar.gz"
-default['Subread']['url'] = "http://sourceforge.net/projects/subread/files/subread-#{node['Subread']['version']}/#{node['Subread']['filename']}/download"
+default['Subread']['url'] = "http://sourceforge.net/projects/subread/files/subread-#{node['Subread']['version']}/#{node['Subread']['filename']}"
 default['Subread']['install_path'] = '/usr/local'
 default['Subread']['bin_path'] = '/usr/local/bin'
