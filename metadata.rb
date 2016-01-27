4f5242c028aea92d6c89b8f8bcf1c31539cb33e0
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-27 10:54:59 +0000

    Standardising license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'Subread'
 maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'apache2'
+license 'Apache v2.0'
 description 'Installs/Configures Subread'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
